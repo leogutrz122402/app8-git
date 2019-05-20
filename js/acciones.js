5745364ab37ef032c0e256022a1d8c0ced1c932b
ikesilvestre151 <38224639+ikesilvestre151@users.noreply.github.com>
2019-05-20 13:44:03 -0500

    app8

---
+++ js/acciones.js
@@ -0,0 +1,31 @@
+//JavaScript 
+
+$(document).ready(function(e) {
+    document.addEventListener("deviceready",onDeviceReady, false);
+	
+});
+
+function onDeviceReady(){
+	
+	$('#posicion').on('click',function(){
+		getPosition();
+	});
+}
+
+function getPosition(){
+	var options={
+		enableHighAccaracy :true,
+		maximunAge:3600000
+	}
+	
+	var watchID=Navigator.geolocation.getCurrentPosition(onSuccess, onError, options);
+	
+	function onSuccess(position){
+		alert('Latitude:'  +pósition.coords.latitude  +'\n'+
+		'Longitude:'  +position.coords.longitude  +'\n');
+	}
+	function onError(error){
+		alert('code:'  +error.code  +'\n' +'message:'+error.message+'\n');
+	}
+}
+		
